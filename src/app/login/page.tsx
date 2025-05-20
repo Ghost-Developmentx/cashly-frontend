ce5dd5412e32b943b644bc62297d0a1d02372b8f
Ghost <ghostdevelopmentx@protonmail.com>
2025-05-20 18:51:48 -0400

    Add login page and basic middleware

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { SignIn } from '@clerk/nextjs'
+
+export default function LoginPage() {
+    return (
+    <div className="min-h-screen flex items-center justify-center">
+      <SignIn path="/login" />
+    </div>
+  );
+}
